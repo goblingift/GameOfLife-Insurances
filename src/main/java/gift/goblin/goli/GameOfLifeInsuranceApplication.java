d5e07fb82194ba0ac911962f2bdfe12385aaf789
André Keßler <andre.kessler91@gmx.de>
2021-06-03 13:53:11 +0200

    Initial commit

---
+++ src/main/java/gift/goblin/goli/GameOfLifeInsuranceApplication.java
@@ -0,0 +1,13 @@
+package gift.goblin.goli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameOfLifeInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameOfLifeInsuranceApplication.class, args);
+	}
+
+}
